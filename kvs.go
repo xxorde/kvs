c643cd97226900575b951007295826df4e8c4272
Alexander Sosna <alexander.sosna@credativ.de>
2015-10-02 16:18:41 +0200

    add test :)

--- kvs.go
+++ kvs.go
@@ -6,16 +6,22 @@ import (
 	"io"
 	"os"
 	"sync"
+	"time"
 )
+
+type Tupel struct {
+	Value string
+	Ttl time.Time
+}
 
 type Kvs struct {
 	sync.RWMutex
-	M map[string]string
+	M map[string]Tupel
 }
 
 func NewKvs() *Kvs {
 	kvs := new(Kvs)
-	kvs.M = make(map[string]string)
+	kvs.M = make(map[string]Tupel)
 	return kvs
 }
 
@@ -29,23 +35,44 @@ func (s *Kvs) Len() int {
 	return len(s.M)
 }
 
-func (s *Kvs) Put(key string, value string) {
+func (s *Kvs) PutTtl(key string, value string, ttl time.Time) {
 	s.Lock()
-	s.M[key] = value
-	s.Unlock()
+	defer s.Unlock()
+	tmpTupel := s.M[key]
+	tmpTupel.Value = value
+	tmpTupel.Ttl = ttl
+	s.M[key] = tmpTupel
 }
 
-func (s *Kvs) Get(key string) (value string) {
+func (s *Kvs) Put(key string, value string) {
+	var ttl time.Time
+	s.PutTtl(key, value, ttl)
+}
+
+func (s *Kvs) Get(key string) (string) {
 	s.RLock()
-	value = s.M[key]
-	s.RUnlock()
-	return value
+	defer s.RUnlock()
+	tmpTupel := s.M[key]
+	tmpTtl := tmpTupel.Ttl
+	if tmpTtl.Second() == 0 {
+		return tmpTupel.Value
+	}
+	delta := tmpTtl.Sub(time.Now())
+	if(delta.Nanoseconds() > 0) {
+		// Value is still valid
+		return tmpTupel.Value
+	} else {
+		// delete the key
+		go s.Delete(key)
+		// return empty string, because the value is no longer valid
+		return ""
+	}
 }
 
 func (s *Kvs) Delete(key string) {
 	s.Lock()
+	defer s.Unlock()
 	delete(s.M, key)
-	s.Unlock()
 }
 
 func (s *Kvs) Exists(key string) (value bool) {
---
+++ kvs_test.go
@@ -0,0 +1,79 @@
+package kvs
+
+import (
+	"testing"
+	"os"
+	"time"
+	"fmt"
+//	"math/rand"
+//	"strconv"
+)
+
+var (
+	store Kvs
+	cases []struct {key, value string}
+)
+
+func TestMain(m *testing.M) {
+	// create global kvs
+	store = *NewKvs()
+
+	// just so we use fmt
+	fmt.Println("Test started...")
+
+	// global testcases
+	cases = []struct {key, value string}{
+		{"key","value"},
+		{"Hello", "Welt"},
+		{"Hack", "the Planet"},
+		{"Hack2", "the Planetthe Planetthe Planetthe Planetthe Planetthe Planet"},
+	}
+	os.Exit(m.Run())
+}
+
+func TestPutGet(t *testing.T) {
+	for _, c := range cases {
+		store.Put(c.key,c.value)
+	}
+	tmp := ""
+	for _, c := range cases {
+		tmp = store.Get(c.key)
+		if c.value != tmp {
+			t.Errorf("Get(%q) == %q, should be: %q", c.key, tmp, c.value)
+		}
+	}
+}
+
+func TestPutGetTtl(t *testing.T) {
+	waitTime := time.Duration(1)*time.Second
+
+	// set ttl to n seconds
+	ttl := time.Now()
+	ttl = ttl.Add(waitTime)
+
+	// store values with ttl
+	for _, c := range cases {
+		store.PutTtl(c.key,c.value,ttl)
+	}
+
+	// the values should still be there!
+	tmp := ""
+	for _, c := range cases {
+		tmp = store.Get(c.key)
+		if c.value != tmp {
+			t.Errorf("Get(%q) == %q, should be: %q", c.key, tmp, c.value)
+		}
+	}
+
+	// sleep n secods
+	time.Sleep(waitTime)
+
+	// test if the values are absent!
+	tmp = ""
+	for _, c := range cases {
+		tmp = store.Get(c.key)
+		if c.value == tmp {
+			t.Errorf("Get(%q) == %q, should be: nil", c.key, tmp)
+		}
+	}
+}
